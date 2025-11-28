f987c61101efda25ba19f271352f8ae5f8ff0de9
LucasAhid <lucas.ahid.contato@gmail.com>
2025-11-28 20:45:53 -0300

    Documentação

--- jitterbit-api/src/app.js
+++ jitterbit-api/src/app.js
@@ -7,6 +7,13 @@ app.use(express.json());
 const orderRoutes = require("./routes/orderRoutes");
 app.use("/", orderRoutes);
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/", authRoutes);
+
+const { swaggerUi, swaggerSpec } = require("./swagger");
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("API rodando na porta " + port));
--- jitterbit-api/src/routes/orderRoutes.js
+++ jitterbit-api/src/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
+const authMiddleware = require("../auth/authMiddleware");
 
-router.post("/order", orderController.create);
-router.get("/order/:id", orderController.getOne);
-router.get("/order/list", orderController.getAll);
-router.put("/order/:id", orderController.update);
-router.delete("/order/:id", orderController.remove);
+router.post("/order", authMiddleware, orderController.create);
+router.get("/order/:id", authMiddleware, orderController.getOne);
+router.get("/order/list", authMiddleware, orderController.getAll);
+router.put("/order/:id", authMiddleware, orderController.update);
+router.delete("/order/:id", authMiddleware, orderController.remove);
 
 module.exports = router;
